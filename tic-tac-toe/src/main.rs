421bd5c94227d81733e90cd712f187de5e76e0bb
mkd <mkduer@gmail.com>
2019-05-19 12:55:37 -0700

    wip: setup integration tests

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -194,6 +194,34 @@ fn main() {
 }
 
 
+
+/*****************
+ INTEGRATION TESTS
+******************/
+
+#[cfg(test)]
+mod integration_tests {
+    use super::*;
+    use more_asserts::{assert_ge};
+
+    #[test]
+    fn test_auto_move_reaches_endgame() {
+        // Should result in an endgame within `Game::const SIZE` moves, 
+        // otherwise, there is a halting error
+        let mut max_moves = SIZE as isize;
+        let mut game = Game::new();
+
+        game.start(true, true);
+
+        while (game.end_game == false) && (max_moves > -2) {
+            game.update();
+            max_moves -= 1;
+        }
+        assert_ge!(max_moves, 0)
+    }
+}
+
+
 /***********
  UNIT TESTS
 ************/
@@ -207,6 +235,7 @@ mod tests {
     // TODO: test manual_move
     // TODO: test switch_player
     // TODO: test update()
+
 
     #[test]
     fn test_player_init_settings() {
