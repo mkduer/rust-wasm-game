745764e4705df2aa723269c869a75a80bb9d2c0d
Michelle Duer <michelle.duer@gmail.com>
2019-05-31 19:17:36 -0700

    generate compiled WASM, successful test

--- wasm-tic-tac-toe/src/lib.rs
+++ wasm-tic-tac-toe/src/lib.rs
@@ -1,24 +1,12 @@
-extern crate cfg_if;
 extern crate wasm_bindgen;
-
-use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
-
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
+    pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("greet: alert from lib.rs");
+pub fn greet(alien: &str) {
+    alert(&format!("WASM invasion, fuck yeah! Ooooohhhhh {}!", alien));
 }
