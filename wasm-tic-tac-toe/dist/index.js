a0199fd049f514bd0d7ddf66dcec459af0328aba
Michelle Duer <michelle.duer@gmail.com>
2019-05-31 17:43:47 -0700

    add better variable name

--- wasm-tic-tac-toe/dist/index.js
+++ wasm-tic-tac-toe/dist/index.js
@@ -1,5 +1,5 @@
-const js = import("./node_modules/@mkduer/wasm-tic-tac-toe/");
+const wasm = import("./node_modules/@mkduer/wasm-tic-tac-toe/");
 
-js.then(js => {
-  js.greet();
+wasm.then(js => {
+  wasm.greet();
 });
