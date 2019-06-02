3f7510f19d074a56669d650a077ca356c12dc8c3
Michelle Duer <michelle.duer@gmail.com>
2019-06-01 22:46:16 -0700

    practice code that works

--- wasm-tic-tac-toe/dist/index.js
+++ wasm-tic-tac-toe/dist/index.js
@@ -3,20 +3,27 @@
 *  https://rustwasm.github.io/docs/book/game-of-life/hello-world.html
 */
 
+import * as wasm from "../pkg/wasm_tic_tac_toe";
+
+wasm.greet();
+
+const name = document.getElementById("game-name");
+const pre = document.getElementById("tic-tac-toe-canvas");
+
+name.textContent = wasm.game_name();
+pre.textContent = wasm.mutate(5, 2);
+
+
+
+
+/*
 import { Game } from "../pkg/wasm_tic_tac_toe";
 
 const pre = document.getElementById("tic-tac-toe-canvas");
-/*
 const game = Game.new();
 game.start();
-game.display_indexed_board();
-
 const renderLoop = () => {
-  pre.textContent = game.render();
-  game.update();
-
+  pre.textContent = wasm.mutate(5, 2);
   requestAnimationFrame(renderLoop);
-};
-
-game.reset();
+}
 */
--- wasm-tic-tac-toe/src/lib.rs
+++ wasm-tic-tac-toe/src/lib.rs
@@ -16,6 +16,11 @@ use std::io::{stdin, stdout, Write};
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+pub fn game_name() -> String {
+    "Tic Tac Toe".to_string()
+}
+
 // TODO: remove this test code
 #[wasm_bindgen]
 extern {
@@ -26,4 +31,24 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Testing 1, 2, 3");
+}
+
+#[wasm_bindgen]
+pub fn mutate(a: u32, b: u32) -> u32 {
+    let x = a + 3;
+    let y = b + 1;
+    add(x, y)
+}
+
+fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Tile {
+    Empty = 0,
+    P1 = 1,
+    P2 = 1,
 }
