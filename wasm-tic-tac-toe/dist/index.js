f4a6030833c39ad7203d13c29c06fc165df8fece
Michelle Duer <michelle.duer@gmail.com>
2019-06-07 14:32:12 -0700

    clean html divs, setup for transparent overlay

--- wasm-tic-tac-toe/dist/index.js
+++ wasm-tic-tac-toe/dist/index.js
@@ -18,6 +18,7 @@ function listen() {
   var start_visible = document.getElementById("start-visible");
   var start_collapsed = document.getElementById("start-collapsed");
   var players = document.getElementById("players");
+  var board = document.getElementById("board");
   var game_state = document.getElementById("game-state");
   var winner = document.getElementById("winner");
   var player1_type = true;
@@ -40,7 +41,7 @@ function listen() {
     manual = false;
     game.start(player1_type, player2_type)
     begin(game, reset, start_visible, start_collapsed, 
-          players, game_state, winner, manual)
+          players, board, game_state, winner, manual)
   };
 
   // settings for manual play button
@@ -51,12 +52,12 @@ function listen() {
     manual = true;
     game.start(player1_type, player2_type)
     begin(game, reset, start_visible, start_collapsed, 
-          players, game_state, winner, manual)
+          players, board, game_state, winner, manual)
   };
 }
 
 function begin(game, reset, start_visible, start_collapsed, 
-               players, game_state, winner, manual) {
+               players, board, game_state, winner, manual) {
   // begins the game by rendering it and playing the game ticks
   var manual_dialogue = document.getElementById("manual-dialogue");
   start_visible.style.visibility = "collapse";
@@ -78,7 +79,9 @@ function begin(game, reset, start_visible, start_collapsed,
 }
 
 function render(game, players, board, game_state) {
+  // render game content
   players.textContent = game.render_players();
+  board.style.visibility = "visible";
   board.textContent = game.render_board();
   game_state.textContent = game.render_game_state();
 }
@@ -183,6 +186,7 @@ function reset_all(game, start_visible, start_collapsed, players, board,
   start_visible.style.visibility = "visible";
   start_collapsed.style.visibility = "collapse";
   manual_dialogue.style.visibility = "collapse";
+  board.style.visibility = "collapse";
   players.textContent = "";
   board.textContent = "";
   winner.textContent = "";
