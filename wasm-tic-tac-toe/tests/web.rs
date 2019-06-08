2502c420c851ad0a2e2a2aedf7561ed509c75719
Michelle Duer <michelle.duer@gmail.com>
2019-06-07 20:45:38 -0700

    add source for test example

--- wasm-tic-tac-toe/tests/web.rs
+++ wasm-tic-tac-toe/tests/web.rs
@@ -1,5 +1,8 @@
 //! Test suite for the Web and headless browsers.
 
+// source: sample test grabbed from https://rustwasm.github.io/wasm-pack/book/print.html
+// as reference. These are to be replaced by custom tests relating to this specific project 
+
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
